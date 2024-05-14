9a9a1f721b6cc353fd32c211dce98e4bad9def1d
Mili <milimadeemail@gmail.com>
2024-05-14 10:17:04 -0400

    Added code to filter through text then created a dictionary that counted the number of characters within text

---
+++ main.py
@@ -0,0 +1,37 @@
+def main():
+  book_path = "books/frankenstein.txt"
+  book_text = get_book_text(book_path) 
+  num_of_words = get_num_of_words(book_text)
+  characters_in_text = count_characters(book_path)
+  print(f"--- Begin report of {book_path} ---")
+  print(f"{num_of_words} words found in the document")
+  for character in characters_in_text:
+    print(f"The {character}")
+
+def get_book_text(path):
+  with open(path) as f:
+    return f.read()
+
+def get_num_of_words(text):
+  return len(text.split())
+
+def get_character_count(book_text):
+  characters_in_book = {}
+  word_arr = book_text.split()
+
+def count_characters(path):
+  text = get_book_text(path)
+  word_arr = text.split()
+  characters = {}
+  for word in word_arr:
+    lowercase_word = word.lower()
+    for letter in lowercase_word:
+      if letter in characters:
+        characters[letter] = int(characters[letter]) + 1
+      else:
+        characters[letter] = 1
+  return characters
+  
+main()
+
+
